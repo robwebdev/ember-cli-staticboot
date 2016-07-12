fbd2a47e98f45726b01dcc508c7eb1d2567171f9
Rob Chandler <robkadt@gmail.com>
2016-07-12 14:52:42 +0100

    Improve query params warning

--- addon/components/link-to.js
+++ addon/components/link-to.js
@@ -13,7 +13,10 @@ export default Ember.LinkComponent.extend({
     // Strip out query params
     const splitHref = href.split('?');
     if (splitHref.length > 1) {
-      Ember.Logger.warn('Query params are ignored when using link-to with ember-cli-staticboot');
+      Ember.Logger.warn(`
+        Query params are included in href attributes when using link-to with ember-cli-staticboot.
+        If you require query params this suggests your app is dynamic and not best suited for ember-cli-staticboot.
+      `);
     }
     href = splitHref[0];
 
